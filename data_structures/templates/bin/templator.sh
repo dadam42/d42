2ba8f60b810237becfae21c2040c8cf24bed0a48
damouyal <dadamouyal42@gmail.com>
2020-04-26 00:28:55 +0200

    templator.sh modified to handle type's real name and type tag

--- data_structures/templates/bin/templator.sh
+++ data_structures/templates/bin/templator.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-type="int"
+# The name your type is called in functions' name
+# Essentially usefull if your type is a pointer
+type_tag="int"
+# The name of your type in C
+type_name="int"
 inclusion=""
-sedcommand="s/type([^d])/${type}\1/g ; s/TYPE/${type^^}/g"
+sedcommand="s/type([^d_])/${type_name}\1/g ; s/type([^d])/${type_tag}\1/g ; s/TYPE/${type_tag^^}/g"
 
 for arg in $@
 do
-	filename=$(echo "$arg" | sed "s/type/$type/g" -)
+	filename=$(echo "$arg" | sed "s/type/$type_tag/g" -)
 	if [ ! -z $inclusion ]
 	then
 		sed -E "s/\!d42inclusion/$inclusion/ ; $sedcommand " $arg > $filename
