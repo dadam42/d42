2f4f2822ad52a255f9907a22e72c1534380323ae
damouyal <dadamouyal42@gmail.com>
2020-05-03 17:22:21 +0200

    templator2.sh now handle pointers types correctly

--- templates/bin/templator2.sh
+++ templates/bin/templator2.sh
@@ -28,6 +28,7 @@ do_msg() {
 }
 
 parse_options() {
+	echo "$#"
 	while [[ $# -gt 0 ]]
 	do
 		key="$1"
@@ -57,6 +58,14 @@ parse_options() {
 				shift
 				shift
 			;;
+			-n|--noauto)
+				auto_mode=""
+				shift
+			;;
+			-p|--pointertype)
+				ptr_type=t
+				shift
+			;;
 			-u|--user)
 				user="$2"
 				shift
@@ -82,21 +91,29 @@ parse_options() {
 done
 }
 
+clean_directory_name() {
+	msg "Clearing $1 : ${!1}"
+	declare -g $1=${!1%/}
+	msg "Cleared $1 : ${!1}"
+}
+
 select_source_directory() {
-	source_directory_provided=""
+	here=$(pwd)
 	msg "Selecting source directory : "
 	if [[ -z "$source_directory" ]]
 	then
 		msg "\tNo source directory provided." "\tI will assume you want the result here."
 		source_directory="$here"
 	else
+		source_directory_provided=t
 		if [[ ! -d "$source_directory" ]]
 		then
 			err_exit "\tsource directory : $source_directory is not a directory."
 		fi
-	source_directory_provided="t"
-		msg "\tSource directory is $source_directory"
+		source_directory_provided="t"
 	fi
+	clean_directory_name source_directory
+	msg "\tSource directory is $source_directory"
 }
 
 select_target_directory() {
@@ -110,23 +127,72 @@ select_target_directory() {
 		then
 			err_exit "\ttarget_directory : $target_directory is not a directory."
 		fi
-		msg "\tTarget directory is $target_directory"
 	fi
+	clean_directory_name target_directory
+	msg "\tTarget directory is $target_directory"
 }
 
 select_files() {
-	msg "Selecting files to template :"
-	if [[ $nfiles = 0 ]]
+	local files_provided
+	local files_has_ptr_extension
+	local selected_file
+	local cleaned_name
+	local files_selected
+	local loc_target_filename
+
+	files_provided=""
+	if [[ "$nfiles" = 0 ]]
 	then
+		if [[ -z "$auto_mode" ]]
+		then
+			err_exit "No auto mode but no file provided : can not template :("
+		fi
 		if [[ -z "$source_directory_provided" ]]
 		then
-			err_exit "\tNo file nor directory provided, cannot template :("
-		else
-			do_msg "\tGrabbing all files from $source_directory"
-			files=($source_directory/*)
-			files=($(basename -s '' "${files[@]}"))
+			err_exit "No file nor source directory provided : can not template :("
 		fi
+		do_msg "No file provided, grabbing all files in $source_directory/"
+		files=($source_directory/*)
+		files=($(basename -s '' "${files[@]}"))
+	else
+		files_provided=t
 	fi
+	
+	files_selected=()
+	((filedx_selected=0))
+	for filedx in "${!files[@]}"
+	do
+		msg "Considering selecting ${files[$filedx]}"
+		file_has_ptr_extension=""
+		cleaned_name="${files[$filedx]%\.ptr}"
+		if [[ "${files[$filedx]}" != "$cleaned_name" ]]
+		then
+			file_has_ptr_extension=t
+		fi
+		selected_file="$cleaned_name"
+		if [[ ( ( ! -z "$auto_mode" ) && ( ! -z "$ptr_type" ) ) && ( ( ! -z "$files_provided" ) || ( -z "$file_has_ptr_extension" ) ) ]]
+		then
+			if [[ -f "$source_directory"/"$cleaned_name".ptr ]]
+			then
+				selected_file="$cleaned_name".ptr
+			fi
+		fi
+		if [[ ( ! -z "$auto_mode" ) && ( -z "$files_provided" ) && ( ! -z "$file_has_ptr_extension" ) ]]
+		then
+			msg "Skipping ${files[$filedx]}"
+			continue
+		fi
+		if [[ ( ! -z "$files_provided" ) && ( ! -f "$source_directory"/"$selected_file" ) ]]
+		then
+			do_msg "Must template $selected_file but file does not exist in $source_directory" "\tSkipped"
+			continue
+		fi
+		files_selected[$filedx_selected]="$selected_file"
+		target_filenames[$filedx_selected]="$(echo "$cleaned_name" | sed "s#type#"$type_tag"#g")"
+		msg "Selected $selected_file as ${target_filenames[$filedx_selected]}"
+		((filedx_selected++))
+	done
+	files=("${files_selected[@]}")
 }
 
 parse_type() {
@@ -141,14 +207,18 @@ parse_type() {
 	tmptype=${type%\*}
 	msg "type : $type"
 	msg "tmptype : $tmptype"
-	while [ "$tmptype" != "$type" ]
-	do
-		((nstars++))
-		stars="*""$stars"
-		type="$tmptype"
-		tmptype=${type%\*}
-	done
-	
+	mode=std
+	if [ "$tmptype" != "$type" ]
+	then
+		ptr_type=t
+		while [ "$tmptype" != "$type" ]
+		do
+			((nstars++))
+			stars="*""$stars"
+			type="$tmptype"
+			tmptype=${type%\*}
+		done
+	fi
 	msg "\t type is $type"
 	msg "\t stars is $stars"
 	msg "Parsing type_tag : "
@@ -195,59 +265,66 @@ parse_42header_infos() {
 	fi
 }
 
-target_directory=""
-source_directory=""
+list_selected_files() {
+	local filedx
+
+	if [[ ! -z "$verbose_mode" ]]
+	then
+		do_msg "${#files[@]} files to template : "
+		for filedx in "${!files[@]}"
+		do
+			do_msg "Will template ${files[$filedx]} as ${target_filenames[$filedx]}"
+		done
+	fi
+}
+
+verbose_mode=""
 type=""
 type_tag=""
-stars=""
-((nstars = 0))
+auto_mode=t
+ptr_type=
+files=()
+((nfiles = 0))
+source_directory=""
+source_directory_provided=""
+target_directory=""
 include=""
 user=""
 email=""
-verbose_mode=""
-files=()
-((nfiles = 0))
-
-parse_options $@
-here=$(pwd)
+parse_options "$@"
+stars=""
+((nstars = 0))
+parse_type
+here=""
+source_directory_provided=""
 select_source_directory
 select_target_directory
+target_filenames=()
 select_files
-parse_type
+list_selected_files
 parse_include
 parse_42header_infos
 
 sedcommand='s#type(([[:space:]]{1,})(\**))#'${type}'\1'${stars}'#g ; s#type([^d])#'${type_tag}'\1#g ; s#TYPE#'${type_tag^^}'#g'
-do_msg "sed command :" "$sedcommand"
-for file in "${files[@]}"
+for filedx in "${!files[@]}"
 do
-	filename="$(echo "$file" | sed "s/type/$type_tag/g" -)"
-	target_filename="$target_directory"/"$filename"
-	ctest=${filename%.c}
-	if [[ ! "$filename" = "$ctest" ]]
-	then
-		42header "$filename" "$user" "$email" > "$target_filename"
-	else
-		ctest=${filename%.h}
-		if [[ ! "$filename" = "$ctest" ]]
-		then
-			42header "$filename" "$user" "$email" > "$target_filename"
-		else
-			rm -f "$target_filename"
-		fi
-	fi
-	arg="$source_directory"/"$file"
-	do_msg "Templating $file"
-	if [[ "$arg" = "$target_filename" ]]
+	source_file="$source_directory"/"${files[$filedx]}"
+	target_file="$target_directory"/"${target_filenames[$filedx]}"
+	if [[ "$source_file" -ef "$target_file" ]]
 	then
 		do_msg "Target file and source file are the same."
-		do_msg "Abort templating for $file"
+		do_msg "Abort templating for ${files[$filedx]}."
 		continue
 	fi
+	if [[ ! "${files[$filedx]}" =~ \.(c|h)(.ptr)? ]]
+	then
+		42header "$source_file" "$user" "$email" > "$target_file"
+	fi
+	do_msg "Templating ${files[$filedx]}"
 	if [ ! -z "$include" ]
 	then
-		sed -E "s/\!d42include/$include/ ; $sedcommand " $arg >> "$target_filename"
+		sed -E "s/\!d42include/$include/ ; $sedcommand " "$source_file" >> "$target_file"
 	else
-		sed -E "/\!d42include/d ; $sedcommand " $arg >> "$target_filename"
+		sed -E "/\!d42include/d ; $sedcommand " "$source_file" >> "$target_file"
 	fi
 done
